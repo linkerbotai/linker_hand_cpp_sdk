4fd83666d844af50bc2bb20d4f22db825878c350
liangshaoteng <liangshaoteng2012@163.com>
2025-04-07 18:41:33 +0800

    更新README并加入脚本程序

--- README.md
+++ README.md
@@ -1,12 +1,83 @@
 # LinkerHand-CPP-SDK
 
 ## 概述
-LinkerHand-CPP-SDK 是灵心巧手(北京)科技有限公司开发，用于L10、L20等型号灵巧手的驱动软件和功能示例源码。
+LinkerHand-CPP-SDK 是灵心巧手科技有限公司开发，用于L10、L20等型号灵巧手的驱动软件和功能示例源码。
 
 ## 安装
+#### 依赖环境
+    sudo apt-get install yaml-cpp
+    ...
 
+#### 启动脚本
+    cd linker_hand_cpp_sdk/linker_hand
+    ./script.sh
+    
+![alt text](linker_hand/img/script.png)
+#### 运行示例
+    cd build
+    ./linker_hand_example
 
 ## 快速开始
 
+创建main.cpp文件，并添加以下代码：
+```cpp
+// main.cpp
+#include "LinkerHandApi.h"
+
+int main() {
+    // 初始化机械手
+    LinkerHandApi hand("right", "L10");
+
+    // 设置关节位置
+    std::vector<double> jointAngles = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    hand.setJointPositions(jointAngles);
+
+    // 获取当前状态
+    auto status = hand.getState();
+    std::cout << "Current State: ";
+    for (const auto& val : status) {
+        std::cout << std::hex << (int)val << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+```
+创建CMakeLists.txt文件，并添加以下配置：
+```cmake
+# CMakeLists.txt
+cmake_minimum_required(VERSION 3.5)
+project(MyProject)
+
+# 包含目录
+include_directories(/usr/local/include)
+
+# 添加可执行文件
+add_executable(my_project main.cpp)
+
+# 链接库
+target_link_libraries(my_project /usr/local/lib/liblinker_hand_lib.a pthread)
+```
+
+#### 文件结构
+```
+├── example
+│   ├── CMakeLists.txt
+│   └── main.cpp
+```
+#### 编译
+    cd example
+    mkdir build
+    cd build
+    cmake ..
+    make
+#### 运行
+    ./my_project
+
+
+
+## API文档
+
+[C++ API文档](https://linkerhand.github.io/LinkerHand-CPP-SDK/)
 
 ## 版本更新
---
+++ linker_hand/script.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+VERSION="1.0.0"
+
+function LoadedColor_information()
+{
+  # 文本颜色
+  BLACK='\033[0;30m'
+  # 黑色
+  RED='\033[0;31m'
+  # 红色
+  GREEN='\033[0;32m'
+  # 绿色
+  YELLOW='\033[0;33m'
+  # 黄色
+  BLUE='\033[0;34m'
+  # 蓝色
+  PURPLE='\033[0;35m'
+  # 紫色
+  CYAN='\033[0;36m'
+  # 青色
+  WHITE='\033[0;37m'
+  # 白色
+
+  RED_WHITE='\033[47;31m'
+  # 红色
+  GREEN_WHITE='\033[47;32m'
+  # 绿色
+  WHITE_BLACK='\033[40;37m'
+  # 白色+黑色
+  YELLOW_WHITE='\033[47;33m'
+
+  # 黄色+白色
+  # 背景颜色
+  BLACK_B='\033[40m'
+  # 黑色
+  RED_B='\033[41m'
+  # 红色
+  GREEN_B='\033[42m'
+  # 绿色
+  yellow_B='\033[43m'
+  # 黄色
+  blue_B='\033[44m'
+  # 蓝色
+  purple_B='\033[45m'
+  # 紫色
+  Cyan_B='\033[46m'
+  # 青色
+  White_B='\033[47m'
+  # 白色
+
+  # 文本样式
+  RESET='\033[0m'
+  # 重置所有属性
+  BOLD='\033[1m'
+  # 粗体
+  underline='\033[4m'
+  # 下划线
+  Blinking='\033[5m'
+  # 闪烁
+  Invert='\033[7m'
+  # 反显
+  Hidden='\033[8m'
+  # 隐藏
+#   echo "========================================================="
+#   echo -e "${GREEN}Successfully loaded color information !${RESET}"
+#   echo "========================================================="
+}
+
+CLOSE_RESERVE="exce bash" # exce bash
+
+RUN() {
+	local launch=$1
+	gnome-terminal -- bash -c "source devel/setup.sh; $launch; $CLOSE_RESERVE"
+	sleep 3
+}
+
+
+RUN1() {
+	local launch=$1
+	local command package launch_file
+
+    # 分割launch参数为三个字段
+    IFS=' ' read -r command package launch_file <<< "$launch"
+	
+	gnome-terminal -- bash -c "source devel/setup.sh; $launch; $CLOSE_RESERVE"  > /dev/null 2>&1
+	
+	echo "Node: $package"
+	# 等待节点启动
+    while ! rosnode list | grep -q $package; do
+      echo -e "  启动中..."
+      sleep 1
+    done
+    echo -e "  启动完成"
+}
+
+
+function build_sdk(){
+    mkdir build; cd build; cmake ..; make; cd ..
+}
+
+function install_sdk(){
+    cd build; sudo make install
+}
+
+function uninstall_sdk(){
+    sudo rm -rf /usr/local/lib/liblinker_hand_lib.a
+    sudo rm -rf /usr/local/include/LinkerHandL10Can.h
+    sudo rm -rf /usr/local/include/CanBus.h
+    sudo rm -rf /usr/local/include/HandFactory.h
+    sudo rm -rf /usr/local/include/LinkerHandL20Can.h
+    sudo rm -rf /usr/local/include/LinkerHandApi.h
+    sudo rm -rf /usr/local/include/IHand.h
+    sudo rm -rf /usr/local/config/setting.yaml
+    sudo rm -rf /usr/local/bin/linker_hand_example
+}
+
+function run_example(){
+    cd build; ./linker_hand_example
+}
+
+#------------------------------------------------ 选择菜单 ------------------------------------------
+
+function select_menu(){
+  cd $current_dir
+  echo -e "${GREEN}请根据菜单栏输入你的选择：${RESET}"
+  read -p "" select_num
+  case $select_num in
+      1)
+        echo "编译SDK"
+        build_sdk
+        ;;
+      2)
+      	echo "安装SDK"
+        build_sdk
+        install_sdk
+        ;;
+      3)
+        echo "卸载SDK"
+        uninstall_sdk
+        ;;
+      6)
+        echo "执行示例"
+        run_example
+        ;;
+      0)
+        echo "退出"
+        exit
+        ;;
+      *)
+        echo -e "${RED}输入错误，请重新输入！${RESET}"
+        sleep 1
+        ;;
+  esac
+}
+
+#------------------------------------------------ 菜单栏 ------------------------------------------
+function show_Info(){
+  # 打印顶部边框
+  echo -e "${YELLOW}"
+  echo "================================================"
+  echo -e "${YELLOW} 灵心巧手CPP-SDK  版本：${VERSION} ${RESET}"
+  echo -e "${YELLOW}================================================${RESET}"
+
+  # 打印菜单
+  echo -e "${GREEN}"
+  echo "RUN Choose Task:[请输入括号内的数字]"
+  echo -e "${YELLOW}————————————————————————————————————————————————${RESET}"
+  echo -e "${BLUE}[1]: 编译SDK;${RESET}"
+  echo -e "${YELLOW}————————————————————————————————————————————————${RESET}"
+  echo -e "${BLUE}[2]: 安装SDK;${RESET}"
+  echo -e "${YELLOW}————————————————————————————————————————————————${RESET}"
+  echo -e "${BLUE}[3]: 卸载SDK;${RESET}"
+  echo -e "${YELLOW}————————————————————————————————————————————————${RESET}"
+  echo -e "${RED}[0]: 退出;${RESET}"
+  echo -e "${YELLOW}————————————————————————————————————————————————${RESET}"
+}
+
+#------------------------------------------------ 初始化 ------------------------------------------
+
+#初始化
+function Init()
+{
+	
+    # 加载颜色信息
+    LoadedColor_information
+	# 获取当前文件路径
+  	current_dir=$(pwd)
+    sleep 1
+}
+
+#------------------------------------------------ 主程序 ------------------------------------------
+
+Init
+while true
+do
+    show_Info
+    select_menu
+done
